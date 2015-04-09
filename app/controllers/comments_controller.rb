e982e56749ad63ba7283e92b7508913c93071475
thejdavid <jdlavaud@gmail.com>
2015-04-09 10:25:16 +0200

    wrong attributes name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     # ajout if logged in apres
-   Comment.create!(title:params[:title],content:params[:content],product_id:params[:product_id],user_id:current_user.id)
+   Comment.create!(title:params[:name],content:params[:description],product_id:params[:product_id],user_id:current_user.id)
    redirect_to product_path(params[:product_id])
   end
 end
