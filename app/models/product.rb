4569766ae385b0aab0a322546aa0c1a4a99ac76c
thejdavid <jdlavaud@gmail.com>
2015-04-17 09:11:38 +0200

    test config heroku paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,11 @@ class Product < ActiveRecord::Base
   has_many :order_details
   belongs_to :category
   belongs_to :designer,class_name:"User"
-  has_attached_file :image, :styles => { :medium => "460x>", :thumb => "100x100>",:vnice=> "400x" }
+  has_attached_file :image,
+                    :styles => { :medium => "460x>", :thumb => "100x100>",:vnice=> "400x" }
+                    :storage => :s3,
+                    :bucket => 'remake_asset',
+                    :s3_credentials => S3_CREDENTIALS
   validates_attachment :image, presence: true
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   crop_attached_file :image
