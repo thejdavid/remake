099a22bae3ec1f98bb6fbf93c74fcf636d0c54a9
thejdavid <jdlavaud@gmail.com>
2015-04-17 11:42:19 +0200

    to undo heroku

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :designer,class_name:"User"
   has_attached_file :image,
-                    :styles => { :medium => "460x>", :thumb => "100x100>",:vnice=> "400x" }
+                    :styles => { :medium => "460x>", :thumb => "100x100>",:vnice=> "400x" },
+:storage => :s3,
+ :bucket => 'artnshop',
+ :s3_credentials => "#{Rails.root}/config/aws.yml",
+ :path => "resources/:id/:style/:basename.:extension"
+
   validates_attachment :image, presence: true
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   crop_attached_file :image
